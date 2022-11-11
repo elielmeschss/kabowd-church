40ff5e066cbfa26129f2407135f0f6a79a02898e
elielmeschss <elielmeschss@gmail.com>
2022-11-11 17:29:56 -0300

    new changes

--- src/components/resizing/Image.jsx
+++ src/components/resizing/Image.jsx
@@ -1,40 +1,16 @@
-
 import React from "react";
-import KwdFamilia from "../../assets/images/banner-familia-culto.png"
 
-import HomensGp from "../../assets/images/homens-gp-kwd.png"
+export default function Image({ src, dataName, dataTag, dataClass, maxHeight }) {
 
-function Img({src, data_tag, data_name, maxWidth, ...props }){
+  // if(window.innerWidth < 769){
+  //   maxHeight = 250
+  // }else if(window.innerWidth < 1024 && window.innerWidth > 768  ) {
+  //   maxHeight = 350
+  // }
+
+
   return (
-    <img src={src} className={data_name} data-tag={data_tag} width={maxWidth}/>
-  )
-}
-
-
-export default function Image(){
-
-
-  const normal = KwdFamilia
-  const mobile = HomensGp
-
-  let src
-  let dataTag
-  let maxWidth
-
-  if(window.innerWidth < 768){
-    dataTag = "mobile"
-
-    maxWidth = window.innerWidth
-    src = mobile
-  }else {
-    dataTag = "normal"
-
-    maxWidth = window.innerWidth
-    src = normal
-  }
-
-  return(
-    <Img src={src} data_tag={dataTag} data_name="d-block w-100" maxWidth={maxWidth}/>
+    <img src={src} alt={dataName} data-tag={dataTag} className={dataClass} height={maxHeight} />
   )
 
 }
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -2,12 +2,65 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 import Image from "../resizing/Image"
 
-export default function SliderLayout(){
-  return(
+import KwdFamilia from "../../assets/images/banner-familia-culto.png"
+import KwdQuinta from "../../assets/images/banner-quinta-culto.png"
+import KwdQuarta from "../../assets/images/banner-oracao-culto.png"
+import KwdHomens from "../../assets/images/Homens-honra-mobile.png"
+import KwdMulheres from "../../assets/images/Mulheres-Valor-mobile.png"
+import KwdKids from "../../assets/images/Kabowd-Kids-mobile.png"
+
+const mediasQueries = [
+  {
+    url: KwdFamilia,
+    name: "Culto Família",
+    tag: "desktop",
+    class: "image-item-desktop"
+  }, {
+    url: KwdQuarta,
+    name: "Culto Quarta",
+    tag: "desktop",
+    class: "image-item-desktop"
+  }, {
+    url: KwdQuinta,
+    name: "Culto Quinta",
+    tag: "desktop",
+    class: "image-item-desktop"
+  }, {
+    url: KwdHomens,
+    name: "Culto Família",
+    tag: "mobile",
+    class: "image-item-mobile"
+  }, {
+    url: KwdMulheres,
+    name: "Culto Quarta",
+    tag: "mobile",
+    class: "image-item-mobile"
+  }, {
+    url: KwdKids,
+    name: "Culto Quinta",
+    tag: "mobile",
+    class: "image-item-mobile"
+  }]
+
+const innerWidth = window.innerWidth < 1024
+
+export default function SliderLayout() {
+
+  return (
     <Carousel>
-      <Carousel.Item>
-        <Image/>
-      </Carousel.Item>
+      {
+        mediasQueries.filter((item) => {
+          if (item.tag === 'mobile' && innerWidth) {
+            return item.url
+          } else if (item.tag === 'desktop' && !innerWidth) {
+            return item.url
+          }
+        }).map((newurl, i) => (
+          <Carousel.Item key={i}>
+            <Image src={newurl.url} dataName={newurl.name} dataTag={newurl.tag} dataClass={`d-block w-100 ${newurl.class}`} />
+          </Carousel.Item>
+        ))
+      }
     </Carousel>
   )
 }
