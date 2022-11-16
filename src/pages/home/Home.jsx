0b0c94de47b929016ec2ae27ed1824b63c8a08e6
eliel <elielmeschss@gmail.com>
2022-11-16 06:27:37 -0300

    marquee

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Marquee from '../../components/marquee/Marquee'
 import SliderLayout from "../../components/slider/Slider";
 
-export default function Home(){
-  return(
-    <div>
-      <SliderLayout/>
+export default function Home() {
+  return (
+    <div className="kwd-content-home">
+      <SliderLayout />
+     <Marquee/>
       <h1>Olá Mundo!</h1>
       <p>Home</p>
     </div>
--- src/views/static/header/login/Login.jsx
+++ src/views/static/header/login/Login.jsx
@@ -7,7 +7,7 @@ export default function Login(){
   return(
     <div className="content-header-inner kwd-inner-login">
       <div className="login-inner">
-          <button>Login</button>
+          <button type="submit">Login</button>
           <img src={iconlog} alt="Icone Login" />
       </div>
     </div>
